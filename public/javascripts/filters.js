959ffea6e0fe89dbd9db6b15bde6a0bee0be2171
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-09 13:01:30 +0100

    filtering works with places

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -1,3 +1,34 @@
 'use strict';
 
-const filter = document.getElementsByClassName('filters');
+filterSelection("all");
+
+function filterSelection(c) {
+
+ const x = document.getElementsByClassName("place");
+  if (c == "all") {
+      c = "";
+  }
+  for (let i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+function addClass(element, name) {
+    const arr1 = element.className.split(" ");
+    const arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+  }
+  
+  function removeClass(element, name) {
+    const arr1 = element.className.split(" ");
+    const arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      }
+    }
+    element.className = arr1.join(" ");
+  }
