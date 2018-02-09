cc7abb0682f14891222ba293debee258d7420ae9
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-09 16:14:20 +0100

    file upload working

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -1,34 +1,33 @@
 'use strict';
 
-filterSelection("all");
+filterSelection('all');
 
-function filterSelection(c) {
-
- const x = document.getElementsByClassName("place");
-  if (c == "all") {
-      c = "";
+function filterSelection (c) {
+  const x = document.getElementsByClassName('place');
+  if (c == 'all') {
+    c = '';
   }
   for (let i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    removeClass(x[i], 'show');
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
   }
 }
 
-function addClass(element, name) {
-    const arr1 = element.className.split(" ");
-    const arr2 = name.split(" ");
-    for (let i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+function addClass (element, name) {
+  const arr1 = element.className.split(' ');
+  const arr2 = name.split(' ');
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) { element.className += ' ' + arr2[i]; }
+  }
+}
+
+function removeClass (element, name) {
+  const arr1 = element.className.split(' ');
+  const arr2 = name.split(' ');
+  for (let i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
-  
-  function removeClass(element, name) {
-    const arr1 = element.className.split(" ");
-    const arr2 = name.split(" ");
-    for (let i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);     
-      }
-    }
-    element.className = arr1.join(" ");
-  }
+  element.className = arr1.join(' ');
+}
