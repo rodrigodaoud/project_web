8f2055129fa3454d5fe38a07547ea1d1f38e098e
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-12 11:44:04 +0100

    working on user image stuff

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads/' });
 
 const User = require('../models/user');
 
