b0926e0c3f2cd70f49453db102df74142d0c6c2d
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-14 18:59:08 +0100

    up

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -50,3 +50,28 @@ function removeClass (element, name) {
   }
   element.className = arr1.join(' ');
 }
+
+// Thumbnail Slider
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
